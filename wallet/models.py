ccde12ff4b617d6150af809baf63d87f0fc6ca0e
hamza <mjsule18@gmail.com>
2026-04-18 16:23:03 +0100

    Step 3 Wallet System

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import User
+
+class Wallet(models.Model):
+    CURRENCY_CHOICES = [
+        ('NGN', 'Nigerian Naira'),
+        ('XAF', 'Central African CFA'),
+        
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.currency}"
 
 # Create your models here.
