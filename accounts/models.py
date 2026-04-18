a92930bf3d9b508a85427937b9d25eae657c5396
hamza <mjsule18@gmail.com>
2026-04-18 16:08:12 +0100

    Step 2 Custom user Model (Business users)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_business = models.BooleanField(default=True)
+    company_name = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=100)
+    
 
 # Create your models here.
