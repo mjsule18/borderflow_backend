7608de969763bf6ee08dfc7f0d9b8fcc6764be12
hamza <mjsule18@gmail.com>
2026-04-19 16:39:44 +0100

    Step 5 Simple FX Conversion Logic

---
+++ transactions/utils.py
@@ -0,0 +1,4 @@
+def convert_ngn_to_xaf(amount_ngn):
+    exchange_rate = 0.0025  # Example exchange rate, you should replace this with a real one
+    amount_xaf = amount_ngn * exchange_rate
+    return amount_xaf
