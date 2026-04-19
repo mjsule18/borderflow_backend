14f13e45f9fbef6c6887b4d9a2a8c88017470f7a
hamza <mjsule18@gmail.com>
2026-04-19 16:31:57 +0100

    Step 4 Transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import User
 
+
+class Transaction(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('COMPLETED', 'Completed'),
+        ('FAILED', 'Failed'),
+    ]
+
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_transactions')
+    receiver_name = models.CharField(max_length=255)
+    receiver_country = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    currency_from = models.CharField(max_length=3)
+    currency_to = models.CharField(max_length=3)
+    exchange_rate = models.DecimalField(max_digits=12, decimal_places=6)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
 # Create your models here.
