86c8dea9fceb371362be314c040224237e070a40
hamza <mjsule18@gmail.com>
2026-04-22 14:34:46 +0100

    Step 6 Basic API (Django Rest Framework)

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Transaction
+from .utils import convert_ngn_to_xaf
+
+@api_view(['POST'])
+def send_money(request):
+    amount = float(request.data['amount'])
+    receiver = request.data.get('receiver')
+    
+    converted, rate = convert_ngn_to_xaf(amount)
+
+    txn = Transaction.objects.create(
+        sender=request.user,
+        receiver_name=receiver,
+        receiver_country="Cameroon",
+        amount=amount,
+        currency_from="NGN",
+        currency_to="XAF",
+        exchange_rate=rate,
+        status="pending"
+    )
+    return Response({"message": "Transaction created", "xaf_amount": converted, "rate": rate })
+
+
 
 # Create your views here.
